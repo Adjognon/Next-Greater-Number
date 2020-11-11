2627a5ae7b37253c45860cbc420636ae48b1238d
Michel ADJOGNON <kadjogno@isi-mtl.com>
2020-11-10 21:09:16 -0500

    Le chiffre le plus petit que le précedent, de droite vers la gauche

--- php/NextGreaterNumber.php
+++ php/NextGreaterNumber.php
@@ -13,6 +13,18 @@ class NextGreaterNumber{
         $this->indexTo = ($this->number_length - 1);
         $this->min_index = '';
     }
+
+    public function next_greater_number(){
+        $output = '';
+
+        for($i = ($this->number_length - 1); $i >= 0; $i--){
+            if($this->number[$i] > $this->number[$i - 1] && $i > 0){;
+               $this->min_index = ($i - 1);
+            break;
+            }
+        }
+
+    }
 }
 
 ?>
